537062cc362ef323e82e374addc66c00d6bbd968
Okke van Eck <okke.van.eck@gmail.com>
2020-02-27 15:19:36 +0100

    Added checks for placement and fixed the checks for file data

--- amstelhaege/__init__.py
+++ amstelhaege/__init__.py
@@ -11,11 +11,16 @@ does so by doing the following tests in this order:
 """
 
 import check50
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, MultiPolygon
 import pandas as pd
 import numpy as np
 import os
 import re
+
+
+# Define common used variables.
+TYPES = ["WATER", "EENGEZINSWONING", "BUNGALOW", "MAISON"]
+CORNER_LABELS = [f"corner_{x}" for x in range(1, 5)]
 
 
 @check50.check()
@@ -35,10 +40,6 @@ def check_file():
     with open("output.csv") as csvfile:
         df = pd.read_csv(csvfile)
 
-        # Define common used variables.
-        types = ["WATER", "EENGEZINSWONING", "BUNGALOW", "MAISON"]
-        corner_labels = [f"corner_{x}" for x in range(1, 5)]
-
         # Check header for correct format.
         if list(df) != ["structure", "corner_1", "corner_2", "corner_3",
                         "corner_4", "type"]:
@@ -47,7 +48,7 @@ def check_file():
                                   "corner_4,type'")
 
         # Check footer for correct format.
-        if len(df) < 1 or df['structure'].iloc[-1] != "score":
+        if len(df) < 1 or df["structure"].iloc[-1] != "score":
             raise check50.Failure("Expected last row of the csv to be "
                                   "'score,<integer>'")
 
@@ -62,7 +63,7 @@ def check_file():
             return
 
         # Check if all types are correct.
-        type_bools = df["type"][:-1].isin(types).values
+        type_bools = df["type"][:-1].isin(TYPES).values
         if False in type_bools:
             idxs = np.where(type_bools == False)[0]
             error = "Invalid TYPE(s) used for objects. Expected 'WATER', " \
@@ -88,18 +89,18 @@ def check_file():
             raise check50.Failure(error)
 
         # Check if the percentage of different houses are correct.
-        # perc = round(df['type'][:-1][df.type != "WATER"]
-        #              .value_counts(normalize=True) * 100).astype(int)
-        # if perc["EENGEZINSWONING"] != 60 or perc["BUNGALOW"] != 25 or \
-        #         perc["MAISON"] != 15:
-        #     raise check50.Failure("Percentage of different houses are "
-        #                           "incorrect")
+        perc = round(df['type'][:-1][df.type != "WATER"]
+                     .value_counts(normalize=True) * 100).astype(int)
+        if perc["EENGEZINSWONING"] != 60 or perc["BUNGALOW"] != 25 or \
+                perc["MAISON"] != 15:
+            raise check50.Failure("Percentage of different houses are "
+                                  "incorrect")
 
         # Check if all values in the coordinate columns are of correct datatype
         # and value, except for the last row.
         pattern = r"\(([0-9]|[1-9][0-9]*),([0-9]|[1-9][0-9]*)\)"
 
-        for pos in corner_labels:
+        for pos in CORNER_LABELS:
             coord_bools = list(map(lambda x: bool(re.match(pattern, x)),
                                    df[pos][:-1]))
             if False in coord_bools:
@@ -120,7 +121,7 @@ def check_file():
         correct_areas = {"WATER": 0.0, "EENGEZINSWONING": 64.0,
                          "BUNGALOW": 77.0, "MAISON": 120.0}
 
-        for s_type, area in zip(types, correct_areas.values()):
+        for s_type, area in zip(TYPES, correct_areas.values()):
             for row in df[:-1].loc[df['type'] == s_type].values:
                 p = Polygon(tuple(map(float, c[1:-1].split(",")))
                             for c in row[1:-1])
@@ -134,7 +135,7 @@ def check_file():
                     " a rectangle with correct area, but found area of:\n"
 
             for s in inv_structures:
-                idx = df[df['structure'] == s[1]].index.tolist()[0]
+                idx = df[df["structure"] == s[1]].index.tolist()[0]
                 error = "".join([error, f"\t{s[2]} instead of "
                                         f"{correct_areas[s[0]]}\t for '{s[1]}'"
                                         f" on row {idx}\n"])
@@ -145,16 +146,74 @@ def check_file():
 @check50.check(check_file)
 def check_placement():
     """Check if all objects are placed correctly."""
-    pass
+    with open("output.csv") as csvfile:
+        df = pd.read_csv(csvfile)
 
-    # TODO: implement the following checks:
-    #   - Are all houses on the 160 x 180 grid?
-    #       - Keep rotation in mind.
-    #   - Are no houses placed on water?
-    #   - Are the vrijstand of the houses at least: (also checks if houses are placed onto each other)
-    #       - 2m for EENGEZINSWONING
-    #       - 3m for BUNGALOW
-    #       - 6m for MAISON
+        # Create Polygons from the coordinates and check if they overlap.
+        ps_water = {}
+        ps_houses = {}
+        intersects = []
+
+        for row in df[:-1].values:
+            p = Polygon(tuple(map(float, c[1:-1].split(","))) for c in row[1:-1])
+
+            if p.intersects(MultiPolygon(list(ps_water.values()))):
+                intersects.append([row[0], "Water"])
+
+            if p.intersects(MultiPolygon(list(ps_houses.values()))):
+                intersects.append([row[0], "House"])
+
+            if row[-1] == "WATER":
+                ps_water[row[0]] = p
+            else:
+                ps_houses[row[0]] = p
+
+        if intersects:
+            error = "Structures may not overlap, but found:\n"
+
+            for s in intersects:
+                idx = df[df["structure"] == s[0]].index.tolist()[0]
+                error = "".join([error, f"\t{s[1]} was overlapped by '{s[0]}' "
+                                        f"\ton row {idx}\n"])
+
+            raise check50.Failure(error)
+
+        # Check if the area of the total map is 160x180.
+        polys = list(ps_water.values())
+        polys.extend(list(ps_houses.values()))
+        area = MultiPolygon(polys).area
+
+        if area > 28800.0:
+            raise check50.Failure(f"Dimensions of the map are not 160x180: "
+                                  f"'{area}' exceeds 28800.")
+
+        free_space = {}
+        house_polys = list(ps_houses.values())
+        min_extra_meters = [0.0, 2.0, 3.0, 6.0]
+        invalid_houses = np.array([[0, 0, 0]], ndmin=2)
+
+        # Check if the minimal extra meters for houses are correct.
+        for s, p in ps_houses.items():
+            other_houses = list(house_polys)
+            other_houses.remove(p)
+            free_space[s] = p.distance(MultiPolygon(other_houses))
+            s_type = df[df["structure"] == s]["type"].values[0]
+            req_space = min_extra_meters[TYPES.index(s_type)]
+
+            if req_space > free_space[s]:
+                invalid_houses = np.append(invalid_houses,
+                                           [[s, free_space[s], req_space]],
+                                           axis=0)
+
+        if invalid_houses[1:].size:
+            error = "Structures with less than minimal free meters found:\n"
+
+            for s, space, req_space in invalid_houses[1:]:
+                error = "".join([error, f"\t'{s}' \t has {space} free meters "
+                                        f"instead of the required "
+                                        f"{req_space}\n"])
+
+            raise check50.Failure(error)
 
 
 @check50.check(check_placement)
