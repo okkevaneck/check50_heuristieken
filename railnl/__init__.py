e57ee6b57313e16aeac5a000c772a1dde6dee6fc
Okke van Eck <okke.van.eck@gmail.com>
2020-03-03 14:50:43 +0100

    Fixed indents

--- railnl/__init__.py
+++ railnl/__init__.py
@@ -30,36 +30,37 @@ def check_file():
         raise check50.Failure("Output.csv may not be empty. Provide at least "
                               "an header row and a row with a score.")
 
-    with open("output.csv") as csvfile:
-        df = pd.read_csv(csvfile)
+    df = pd.read_csv(open("output.csv"))
 
-        # Check header for correct format.
-        if list(df) != ["train", "stations"]:
-            raise check50.Failure("Expected header of the csv to be "
-                                  "'train,stations'")
+    # Check header for correct format.
+    if list(df) != ["train", "stations"]:
+        raise check50.Failure("Expected header of the csv to be "
+                              "'train,stations'")
 
-        # Check footer for correct format.
-        if len(df) < 1 or df['train'].iloc[-1] != "score" or \
-                df['stations'].iloc[-1].dtype != int:
-            raise check50.Failure("Expected last row of the csv to be "
-                                  "'score,<integer>'")
+    # Check footer for correct format.
+    if len(df) < 1 or df['train'].iloc[-1] != "score" or \
+            df['stations'].iloc[-1].dtype != int:
+        raise check50.Failure("Expected last row of the csv to be "
+                              "'score,<integer>'")
 
-        # Stop checking if there are no tracks are in the output file.
-        if len(df) == 1:
-            return
+    # Stop checking if there are no tracks are in the output file.
+    if len(df) == 1:
+        return
 
-        # Check if all train names are unique.
-        dup_bools = np.array(df["train"].duplicated())
-        if True in dup_bools:
-            idxs = np.where(dup_bools == True)[0]
-            error = "Expected all train names to be unique, but found " \
-                    "duplicate:\n"
+    # Check if all train names are unique.
+    dup_bools = np.array(df["train"].duplicated())
+    if True in dup_bools:
+        idxs = np.where(dup_bools == True)[0]
+        error = "Expected all train names to be unique, but found " \
+                "duplicate:\n"
 
-            for idx in idxs:
-                error = "".join([error, f"\t'{df['train'][idx]}' \ton row "
-                                        f"{idx}.\n"])
+        for idx in idxs:
+            error = "".join([error, f"\t'{df['train'][idx]}' \ton row "
+                                    f"{idx}.\n"])
 
-            raise check50.Failure(error)
+        raise check50.Failure(error)
 
-        # Check if stations are lists of strings.
-        
+    # Check if list  of stations is formated correctly and the station
+    # exists in StatinosNatinoal.csv.
+    existing_stations = df = pd.read_csv(open("/data/StationsNational.csv"))
+    # coord_bools = np.array(list(map(lambda x: bool(re.match(pattern, x)), df[pos][:-1])))
