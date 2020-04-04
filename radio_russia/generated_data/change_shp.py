f39e733c9f05267b9dfe5bec2422f3411ea46523
Okke van Eck <okke.van.eck@gmail.com>
2020-04-04 11:31:05 +0200

    Fixed the input files for russia

--- radio_russia/generated_data/change_shp.py
+++ radio_russia/generated_data/change_shp.py
@@ -4,7 +4,7 @@ from shapely.ops import cascaded_union
 import fiona
 
 if __name__ == '__main__':
-    geo_df = gp.read_file("raw/usa_updated/usa_regions.shp")
+    geo_df = gp.read_file("raw/russia_updated/russia_regions_updated.shp")
     geo_df_vals = geo_df.geometry.tolist()
 
     new_geos = []
@@ -12,23 +12,23 @@ if __name__ == '__main__':
 
     for i, poly in enumerate(geo_df_vals):
         # Delete these.
-        if i in [33]:
+        if i in [11]:
             continue
 
-        # # Merge 45,46,47.
+        # Merge 45,46,47.
         # if i == 45:
         #     new_geos.append(cascaded_union([poly, geo_df_vals[46], geo_df_vals[47]]))
         #     merges.append(46)
         #     merges.append(47)
         #     continue
-        #
-        # # Create the merges.
+
+        # Create the merges.
         # if i in [5,7,10,15,17,19,22,24,26,28,30,33,40,43,51,53]:
         #     new_geos.append(cascaded_union([poly, geo_df_vals[i+1]]))
         #     merges.append(i+1)
         #     continue
-        #
-        # # These are merged
+
+        # These are merged
         # if i in merges:
         #     continue
 
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     print(new_geos)
     print(new_df)
 
-    new_df.to_file("raw/usa_updated/usa_regions_updated.shp")
+    new_df.to_file("raw/russia_updated/russia_regions_updated.shp")
