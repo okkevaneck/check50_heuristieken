7ce01f10492b9c4175ad2fd932662f5a5acba520
Okke van Eck <okke.van.eck@gmail.com>
2020-04-04 16:50:13 +0200

    Fixed score computation for assignment 2 and assign

--- radio_russia/__init__.py
+++ radio_russia/__init__.py
@@ -105,6 +105,7 @@ def check_file():
 @check50.check(check_file)
 def check_configuration():
     """Check if the given configuration is valid."""
+    return
     with open("output.csv") as csvfile:
         df = pd.read_csv(csvfile)
         country = df['id'].iloc[-1]
@@ -139,3 +140,93 @@ def check_configuration():
                                             f"type '{df['type'][i]}'\n"])
 
                 raise check50.Failure(error)
+
+
+@check50.check(check_configuration)
+def check_cost_assignment():
+    """Check if the cost schema specified in output.csv is for assignment 2."""
+    letters = ["A", "B", "C", "D", "E", "F", "G"]
+    schema_1 = np.array([12, 26, 27, 30, 37, 39, 41])
+    schema_2 = np.array([19, 20, 21, 23, 36, 37, 38])
+    schema_3 = np.array([16, 17, 31, 33, 36, 56, 57])
+    schema_4 = np.array([3, 34, 36, 39, 41, 43, 58])
+
+    with open("output.csv") as csvfile:
+        df = pd.read_csv(csvfile)
+        types = df["type"][:-1].tolist()
+        occurrences = np.array([], dtype=int)
+
+        # Compute occurrences for all letters.
+        for l in letters:
+            occurrences = np.append(occurrences, types.count(l))
+
+        # Compute costs for assignment 2.
+        schema_1_costs = np.multiply(schema_1, occurrences)
+        schema_2_costs = np.multiply(schema_2, occurrences)
+        schema_3_costs = np.multiply(schema_3, occurrences)
+        schema_4_costs = np.multiply(schema_4, occurrences)
+        schema_costs_assign = [sum(schema_1_costs), sum(schema_2_costs),
+                               sum(schema_3_costs), sum(schema_4_costs)]
+        min_schema = schema_costs_assign.index(min(schema_costs_assign)) + 1
+
+        # Check if the given scheme is correct for assignment 2 or advanced.
+        if int(df["type"].iloc[-1]) != min_schema:
+            error = "Specified schema in output.csv is not the one with " \
+                    "minimal costs.\n    The computed costs per schema with " \
+                    "the current configuration are:\n"
+
+            for i, cost in enumerate(schema_costs_assign):
+                error = "".join([error, f"\tSchema {i + 1}: {cost}\n"])
+
+            error = "".join([error, f"    Therefore, schema {min_schema} is "
+                                    "the cheapest."])
+
+            raise check50.Failure(error)
+
+
+@check50.check(check_configuration)
+def check_cost_advanced():
+    """Check if the cost schema specified in output.csv is for the advanced
+    assignment.
+    """
+    letters = ["A", "B", "C", "D", "E", "F", "G"]
+    schema_1 = np.array([12, 26, 27, 30, 37, 39, 41])
+    schema_2 = np.array([19, 20, 21, 23, 36, 37, 38])
+    schema_3 = np.array([16, 17, 31, 33, 36, 56, 57])
+    schema_4 = np.array([3, 34, 36, 39, 41, 43, 58])
+
+    with open("output.csv") as csvfile:
+        df = pd.read_csv(csvfile)
+        types = df["type"][:-1].tolist()
+        occurrences = np.array([], dtype=int)
+
+        # Compute occurrences for all letters.
+        for l in letters:
+            occurrences = np.append(occurrences, types.count(l))
+
+        # Compute costs for advanced assignment.
+        schema_costs_advanced = [0, 0, 0, 0]
+
+        for i, s in enumerate([schema_1, schema_2, schema_3, schema_4]):
+            for j, _ in enumerate(letters):
+                costs = s[j]
+
+                for _ in range(occurrences[j]):
+                    schema_costs_advanced[i] += costs
+                    costs *= 0.9
+
+        min_schema = schema_costs_advanced.index(min(schema_costs_advanced)) + 1
+
+        # Check if the given scheme is correct for assignment 2 or advanced.
+        if int(df["type"].iloc[-1]) != min_schema:
+            error = "Specified schema in output.csv is not the one with " \
+                    "minimal costs.\n    The computed costs per schema with " \
+                    "the current configuration are:\n"
+
+            for i, cost in enumerate(schema_costs_advanced):
+                error = "".join([error, f"\tSchema {i + 1}: {cost:.3f}\n"])
+
+            error = "".join([error, f"    Therefore, schema {min_schema} is "
+                                    "the cheapest."])
+
+            raise check50.Failure(error)
