26c05f4a4659484647f960c7e38089653a343e7c
Okke van Eck <okke.van.eck@gmail.com>
2020-06-08 21:44:50 +0200

    Made strings back to usual for imports, but changed to r string

--- railnl/national/__init__.py
+++ railnl/national/__init__.py
@@ -99,7 +99,8 @@ def check_file():
             raise check50.Failure(error)
 
         # Check if all stations in output.csv are specified in stations.csv.
-        with open(os.path.abspath("data/stations.csv")) as stationsfile:
+        # with open(os.path.abspath("data/stations.csv")) as stationsfile:
+        with open(r"data/stations.csv") as stationsfile:
             existing_stations = pd.read_csv(stationsfile)["station"]
             loaded_stations = df["stations"][:-1].map(lambda x: x[1:-1]
                                                       .split(", ")).values
@@ -125,7 +126,8 @@ def check_file():
 def check_tracks():
     """Check if the solution is valid."""
     with open("output.csv") as csvfile, \
-            open(os.path.abspath("data/connections.csv")) as connectionsfile:
+        open(r"connections.csv") as connectionsfile:
+        # open(os.path.abspath("data/connections.csv")) as connectionsfile:
         df = pd.read_csv(csvfile)
         connections = pd.read_csv(connectionsfile)
 
@@ -184,7 +186,9 @@ def check_tracks():
 def check_score():
     """Check if solution produces score specified in output.csv."""
     with open("output.csv") as csvfile, \
-        open(os.path.abspath("data/connections.csv")) as connectionsfile:
+        open(r"connections.csv") as connectionsfile:
+        # open(os.path.abspath("data/connections.csv")) as connectionsfile:
+
         df = pd.read_csv(csvfile)
         connections = pd.read_csv(connectionsfile)
 
