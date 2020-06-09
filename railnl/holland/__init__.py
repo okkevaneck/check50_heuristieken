f890de1d746f5c08aac3510c31004a350bfd1cc7
Okke van Eck <okke.van.eck@gmail.com>
2020-06-09 15:35:26 +0200

    Setup for data folder usage

--- railnl/holland/__init__.py
+++ railnl/holland/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This file uses check50 to check the output of a RailNL solution. It does so by
-executing the checks from the national problem folder. The maximum time per
+executing the checks from the checks problem folder. The maximum time per
 track and maximum number of tracks is changed according to the holland problem.
 It will also use the `stations.csv` and `connections.csv` files stored in the
 holland problem folder.
@@ -9,12 +9,15 @@ holland problem folder.
 @author: Okke van Eck
 @contact: okke.van.eck@gmail.com
 """
-
-national = __import__("check50").import_checks("../national")
-from national import *
+checks = __import__("check50").import_checks("../checks")
+from checks import *
+import check50
 
 # Global from the national test to specify the maximum time in minutes per
 # track and maximum number of tracks. This global is changed according to the
 # holland problem and will be used during the tests.
-national.MAX_TIME = 120
-national.MAX_TRACKS = 7
+checks.MAX_TIME = 120
+checks.MAX_TRACKS = 7
+
+# Include data for the national problem.
+check50.include("../data")
